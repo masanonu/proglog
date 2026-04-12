185ccac375e35c349aae0428c7e87d5768106e9f
scenbee013 <141686579+masanonu@users.noreply.github.com>
2026-04-12 13:11:13 +0900

    Add files via upload

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,9 @@
-
+package main
+import (
+       "log"
+       "github.com/masanonu/proglog/internal/server"
+       )
+func main() {
+     srv := server.NewHTTPServer(":8080")
+     log.Fatal(srv.ListenAndServe())
+}
